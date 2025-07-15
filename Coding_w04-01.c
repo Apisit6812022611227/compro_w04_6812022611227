b17d2fb268ae9ac399c76b808b2051b855853382
XApisit6812022611227 <s6812022611227@email.kmutnb.ac.th>
2025-07-15 21:07:21 +0700

    8888

--- Coding_w04-01.c
+++ Coding_w04-01.c
@@ -1,7 +1,7 @@
-#include <stdio.h>  
+#include <stdio.h>          //    input/output เช่น printf()
 
-int main() {        
-    printf("Hello,\n");      
-    printf("C Tokens!\n");    
-    return 0;       
+int main() {                //    จุดเริ่มต้นของโปรแกรม
+    printf("Hello,\n");     //    แสดงข้อความ Hello
+    printf("C Tokens!\n");  //    แสดงข้อความ C Tokens!
+    return 0;               //    ส่งค่ากลับ 0 เพื่อบอกว่าโปรแกรมทำงานเสร็จสมบูรณ์
 }
